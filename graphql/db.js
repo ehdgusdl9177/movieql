fe1d1f8dea160dedecddd522090152d93f018415
ehdgusdl9177 <k23129177@gmail.com>
2022-02-22 18:39:01 +0900

    Extending the Schema2

---
+++ graphql/db.js
@@ -0,0 +1,31 @@
+export const people = [
+  {
+    id: "1",
+    name: "Nicolas",
+    age: 18,
+    gender: "female",
+  },
+  {
+    id: "2",
+    name: "Kevin",
+    age: 25,
+    gender: "male",
+  },
+  {
+    id: "3",
+    name: "Martin",
+    age: 27,
+    gender: "male",
+  },
+  {
+    id: "4",
+    name: "Charlie",
+    age: 27,
+    gender: "male",
+  },
+];
+
+export const getById = (id) => {
+  const filteredMovies = people.filter((person) => id === people.id);
+  return filteredMovies[0];
+};
