8b5112c926b3b65b0466d3b75134b05ed1ab6fb6
ehdgusdl9177 <k23129177@gmail.com>
2022-02-24 11:22:02 +0900

    Wrapping a REST API with GraphQL Part 2

--- graphql/db.js
+++ graphql/db.js
@@ -1,9 +1,44 @@
-import fetch from "node-fetch";
+import axios from "axios";
+import { async } from "regenerator-runtime";
 
 const API_URL = "https://yts.mx/api/v2/list_movies.json";
 
 export const getMovies = (limit, rating) => {
-  fetch(`${API_URL}`)
-    .then((res) => res.json())
-    .then((json) => json.data.movies);
+  const {
+    data: {
+      data: { movies },
+    },
+  } = await axios(LIST_MOVIES_URL, {
+    params: {
+      limit,
+      minimum_rating: rating,
+    },
+  });
+  return movies;
+};
+
+export const getMovie = async (id) => {
+  const {
+    data: {
+      data: { movie },
+    },
+  } = await axios(MOVIE_DETAILS_URL, {
+    params: {
+      movie_id: id,
+    },
+  });
+  return movie;
+};
+
+export const getSuggestions = async (id) => {
+  const {
+    data: {
+      data: { movies },
+    },
+  } = await axios(MOVIE_SUGGESTIONS_URL, {
+    params: {
+      movie_id: id,
+    },
+  });
+  return movies;
 };
